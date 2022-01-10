9cba99c5a0ded30280fd88be6a6d44d6ac928add
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-10 14:21:56 +0530

    runner - setup

---
+++ 02-concurrency-patterns/01-runner/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	/*
+		initialize the runner with a timeout
+		Assign multiple tasks to the runner
+		Start the runner
+		if all the tasks are completed within the given time, report "success"
+		if the tasks are not completed with the given time, report "timeout"
+		exit if the execution is interrupted by an os interrupt
+	*/
+
+	timeout := 15 * time.Second
+	r := runner.New(timeout)
+	r.Add(createTask(3))
+	r.Add(createTask(5))
+	r.Add(createTask(6))
+
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			fmt.Println("tasks timed out")
+		case runner.ErrInterrupt:
+			fmt.Println("interrupt received")
+		}
+	} else {
+		fmt.Println("success")
+	}
+
+}
+
+func createTask(t int) func(int) {
+	return func(id int) {
+		fmt.Printf("Processing - Task #%d\n", id)
+		time.Sleep(time.Duration(t) * time.Second)
+	}
+}
