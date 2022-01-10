04b44a10a904c3f129e337b5ad8b1c511de5791f
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-10 17:07:34 +0530

    pool - assignment - solution

--- 02-concurrency-patterns/02-pool/main.go
+++ 02-concurrency-patterns/02-pool/main.go
@@ -44,6 +44,20 @@ func main() {
 		}(client)
 	}
 	wg.Wait()
+
+	fmt.Println("Second batch of operations")
+	var input string
+	fmt.Scanln(&input)
+	wg = &sync.WaitGroup{}
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
+		go func(client int) {
+			doWork(client, p)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	p.Close()
 }
 
 func doWork(id int, p *pool.Pool) {
@@ -52,8 +66,8 @@ func doWork(id int, p *pool.Pool) {
 		log.Fatalln(err)
 	}
 	defer p.Release(conn)
-	fmt.Printf("Worker : %d, Acquired %d:\n", conn.(*DBConnection).ID)
+	fmt.Printf("Worker : %d, Acquired %d:\n", id, conn.(*DBConnection).ID)
 	//use connection
 	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-	fmt.Printf("Worker Done : %d, Released %d:\n", conn.(*DBConnection).ID)
+	fmt.Printf("Worker Done : %d, Released %d:\n", id, conn.(*DBConnection).ID)
 }
--- 02-concurrency-patterns/02-pool/pool/pool.go
+++ 02-concurrency-patterns/02-pool/pool/pool.go
@@ -1,9 +1,81 @@
 package pool
 
-type struct Pool{
+import (
+	"errors"
+	"fmt"
+	"io"
+	"sync"
+)
 
+type Pool struct {
+	factory   func() (io.Closer, error)
+	resources chan io.Closer
+	mutex     *sync.Mutex
+	closed    bool
 }
 
-/* 
+/*
 Fill in the blanks
 */
+
+var ErrInvalidPoolSize = errors.New("Invalid pool size")
+var ErrPoolClosed = errors.New("Pool closed")
+
+func New(factory func() (io.Closer, error), size int) (*Pool, error) {
+	if size <= 0 {
+		return nil, ErrInvalidPoolSize
+	}
+	return &Pool{
+		factory:   factory,
+		resources: make(chan io.Closer, size),
+		mutex:     &sync.Mutex{},
+		closed:    false,
+	}, nil
+}
+
+func (pool *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-pool.resources:
+		if !ok {
+			return nil, ErrPoolClosed
+		}
+		fmt.Println("Acquire : From Pool")
+		return r, nil
+	default:
+		fmt.Println("Acquire : From Factory")
+		return pool.factory()
+	}
+}
+
+func (pool *Pool) Release(r io.Closer) error {
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+
+	if pool.closed {
+		r.Close()
+		return ErrPoolClosed
+	}
+
+	select {
+	case pool.resources <- r:
+		fmt.Println("Release : In Pool")
+		return nil
+	default:
+		fmt.Println("Release : Close & discard the resource")
+		return r.Close()
+	}
+}
+
+func (pool *Pool) Close() {
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+
+	if pool.closed {
+		return
+	}
+	pool.closed = true
+	close(pool.resources)
+	for r := range pool.resources {
+		r.Close()
+	}
+}
