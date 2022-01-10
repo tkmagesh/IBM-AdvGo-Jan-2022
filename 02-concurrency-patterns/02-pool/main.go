f2c39f96ce600480b7961d759054376d9a75d896
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-10 16:19:36 +0530

    pooling - setup

---
+++ 02-concurrency-patterns/02-pool/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+//resource
+type DBConnection struct {
+	ID int
+}
+
+func (c *DBConnection) Close() error {
+	fmt.Printf("Closing %d\n", c.ID)
+	return nil
+}
+
+//resouce factory
+var IDCounter int
+
+func DBConnectionFactory() (io.Closer, error) {
+	IDCounter++
+	fmt.Printf("DBConnectionFactory : Creating resource %d\n", IDCounter)
+	return &DBConnection{ID: IDCounter}, nil
+}
+
+func main() {
+	p, err := pool.New(DBConnectionFactory /* factory */, 3 /* size */)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	wg := &sync.WaitGroup{}
+	clientCount := 10
+	wg.Add(clientCount)
+	for client := 0; client < clientCount; client++ {
+		go func(client int) {
+			doWork(client, p)
+			wg.Done()
+		}(client)
+	}
+	wg.Wait()
+}
+
+func doWork(id int, p *pool.Pool) {
+	conn, err := p.Acquire()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer p.Release(conn)
+	fmt.Printf("Worker : %d, Acquired %d:\n", conn.(*DBConnection).ID)
+	//use connection
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	fmt.Printf("Worker Done : %d, Released %d:\n", conn.(*DBConnection).ID)
+}
